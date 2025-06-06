cc9501dc686c994a5e9471fdd9391bae5590eb7b
Artem <blinowarteom@icloud.com>
2025-06-06 14:24:36 +0300

    add header component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
     import TheWelcome from '../components/TheWelcome.vue'
     import TheContacts from '../components/TheContacts.vue'
+    import TheHeader from '../components/TheHeader.vue'
 </script>
 
 <template>
     <main>
         <meta charset="UTF-8">
+        <TheHeader/>
         <TheWelcome />
         <TheContacts />
     </main>
